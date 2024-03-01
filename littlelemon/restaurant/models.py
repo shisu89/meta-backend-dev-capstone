d1f992f6dc107551e49ca64180e630e87fddf10a
Shi Su <ssu2@slb.com>
2024-03-01 21:44:15 +0400

    updated models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    number_of_guests = models.IntegerField()
+    booking_date = models.DateField()
+
+    def __str__(self):
+        return self.name
